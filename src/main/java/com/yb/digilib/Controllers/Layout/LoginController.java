a29ba3c9ff389fd4f3baa8fff95a12dcc62dcd25
Yohan Baechlé <baechle.yohan@gmail.com>
2024-08-11 15:30:05 +0200

    chore: login form design

--- src/main/java/com/yb/digilib/Controllers/Layout/LoginController.java
+++ src/main/java/com/yb/digilib/Controllers/Layout/LoginController.java
@@ -1,4 +1,31 @@
 package com.yb.digilib.Controllers.Layout;
 
-public class LoginController {
+import com.yb.digilib.Models.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    public Label usernameLbl;
+    public TextField usernameFld;
+    public PasswordField passwordFld;
+    public Button loginBtn;
+    public Label errorLbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loginBtn.setOnAction(event -> Model.getInstance().getViewFactory().showAdminWindows());
+    }
+
+    private void onLogin() {
+        Stage stage = (Stage) errorLbl.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().getViewFactory().showAdminWindows();
+    }
 }
