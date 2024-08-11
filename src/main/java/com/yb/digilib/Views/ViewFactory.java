755a9d5b010d5e8a7c6e28c89d3a6382410f3cee
Yohan-Baechle <baechle.yohan@gmail.com>
2024-08-11 18:04:58 +0200

    refactoring

--- src/main/java/com/yb/digilib/Views/ViewFactory.java
+++ src/main/java/com/yb/digilib/Views/ViewFactory.java
@@ -16,7 +16,7 @@ public class ViewFactory {
     public AnchorPane getDashboardView() {
         if (dashboardView == null) {
             try {
-                dashboardView = new FXMLLoader(getClass().getResource("/fxml/layout/dashboard.fxml")).load();
+                dashboardView = new FXMLLoader(getClass().getResource("/fxml/layout/dashboard/overview.fxml")).load();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -25,19 +25,20 @@ public class ViewFactory {
     }
 
     public void showLoginWindows() {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/layout/login.fxml"));
-        createStage(loader, false);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/layout/login/login.fxml"));
+        // Taille spécifique pour la fenêtre de connexion
+        createStage(loader, false, 600, 400);
     }
-
 
     public void showAdminWindows() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/admin/admin.fxml"));
         AdminController adminController = new AdminController();
         loader.setController(adminController);
-        createStage(loader, true);
+        // Taille spécifique pour la fenêtre d'administration (tableau de bord)
+        createStage(loader, true, 1200, 800);
     }
 
-    private void createStage(FXMLLoader loader, boolean resizable) {
+    private void createStage(FXMLLoader loader, boolean resizable, double width, double height) {
         Scene scene = null;
 
         try {
@@ -50,12 +51,19 @@ public class ViewFactory {
         stage.setScene(scene);
         stage.setTitle("Digilib");
 
+        // Définir la fenêtre redimensionnable ou non
         stage.setResizable(resizable);
 
+        // Appliquer la méthode sizeToScene pour ajuster la taille de la fenêtre
+        stage.sizeToScene();
+
+        // Optionnel : Forcer la taille si nécessaire
+        stage.setWidth(width);
+        stage.setHeight(height);
+
+        // Afficher la fenêtre
         stage.show();
     }
-
-
 
     public void closeStage(Stage stage) {
         stage.close();
