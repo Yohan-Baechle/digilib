d32d8dac7b5990eb1fc86172c26f7984c17cf9ed
Yohan-Baechle <baechle.yohan@gmail.com>
2024-08-11 15:31:05 +0200

    sidebar design

--- src/main/java/com/yb/digilib/Controllers/Layout/MenuController.java
+++ src/main/java/com/yb/digilib/Controllers/Layout/MenuController.java
@@ -1,4 +1,22 @@
 package com.yb.digilib.Controllers.Layout;
 
-public class MenuController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+    public Button homeBtn;
+    public Button manageBooksBtn;
+    public Button manageMembersBtn;
+    public Button loansReturnsBtn;
+    public Button transactionHistoryBtn;
+    public Button reservationsBtn;
+    public Button settingsBtn;
+    public Button logoutBtn;
+    public Button helpSupportBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
 }
