aa3bab9ca2b48674a442fc11d959815509386c0f
Yohan Baechlé <baechle.yohan@gmail.com>
2024-08-11 15:32:02 +0200

    chore: dashboard homepage design

--- src/main/java/com/yb/digilib/Models/Model.java
+++ src/main/java/com/yb/digilib/Models/Model.java
@@ -1,4 +1,24 @@
 package com.yb.digilib.Models;
 
+import com.yb.digilib.Views.ViewFactory;
+
 public class Model {
+    private static Model model;
+    private final ViewFactory viewFactory;
+
+    private Model() {
+        this.viewFactory = new ViewFactory();
+    }
+
+    public static Model getInstance() {
+        if (model == null) {
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getViewFactory() {
+        return viewFactory;
+    }
 }
+
